5cacff43fb43596159990f55aab6d06eee620dde
Hans Strausl <hstrausl@outward.private>
2016-10-05 14:27:53 -0700

    fixed ignore and added customHeaders

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var PORT = 8012;
 
 var dir = process.argv[2];
 
+kickback.customHeaders = {
+	'css/style.css': {
+		'content-type': 'text/css2'
+	}
+}
+
+kickback.ignore.push('test.txt')
+
 kickback.init(dir, function(){
 	console.log(kickback.files);
-	kickback.start(PORT);
+	kickback.run(PORT);
 });
--- server.js
+++ server.js
@@ -4,21 +4,38 @@ var http = require('http'),
 
 var totalFiles;
 var fileCount = 0;
+
+function normDir(dir, file){
+	var str = '';
+	dir.split(path.sep).forEach(function(step, ind){
+		if (ind > 0) str += step + '/';
+	})
+	file = str + file;
+	return file;
+}
+
 var server = {
 	map: {
 		'': 'index.html', // base
 		404: '404.html'
+	},
+	customHeaders: {
+
 	},
 	ignore: [
 		'.git'
 	],
 	baseDir: process.cwd(),
 	files: {},
+	initialized: false,
 	running: false,
 	port: 8080,
 	init: function(dir, cb){
 		dir = dir || server.baseDir;
 		cb = cb || server.run;
+		if (typeof dir !== 'string') throw new Error('First argument must be a String');
+		if (typeof cb !== 'function') throw new Error('Second argument must be a Function');
+		
 		server.watch(dir);
 		fs.readdir(dir, function(err, files){
 			if (err) throw new Error(err);
@@ -28,15 +45,15 @@ var server = {
 					if (!fs.statSync(dir + path.sep + file).isDirectory()){
 						fs.readFile(dir + path.sep + file, function(err, data){
 							if (err) throw new Error(err);
-							var str = '';
-							dir.split(path.sep).forEach(function(step, ind){
-								if (ind > 0) str += step + '/';
-							})
-							file = str + file;
+							
+							file = normDir(dir, file);
 							
 							server.files[file] = data;
 							if (++fileCount > totalFiles - 1){
-								if (!server.running) cb();
+								if (!server.running){
+									server.initialized = true;
+									cb();
+								}
 							}
 						})
 					} else {
@@ -51,32 +68,40 @@ var server = {
 	},
 	requestHandler: function(request, response){
 		var file = request.url.replace('/', '');
-		console.log('Request for: ', file)
+		console.log('Request for: ', file);
 		if (server.map[file]){
 			file = server.map[file];
+		}
+		if (server.customHeaders[file]){
+			for (var header in server.customHeaders[file]){
+				response.setHeader(header, server.customHeaders[file][header]);
+			}
 		}
 		response.end(server.files[file] || server.files[server.map[404]]); // serve the file data
 	},
 	watch: function(dir){
-		fs.watch(dir, function(err, file){
-			fs.readFile(dir + path.sep + file, function(err, data){
-				if (err && err.code == 'ENOENT'){
-					console.log("File removed: ", file);
-				} else if (err) {
-					throw new Error(err);
-				}
-				var str = '';
-				dir.split(path.sep).forEach(function(step, ind){
-					if (ind > 0) str += step + '/';
+		dir = dir || server.baseDir;
+		if (typeof dir !== 'string') throw new Error('Argument must be a string')
+		
+		fs.watch(dir, function(watcherr, file){
+			if (server.ignore.indexOf(normDir(dir, file)) < 0){
+				fs.readFile(dir + path.sep + file, function(err, data){
+					if (err && err.code == 'ENOENT'){
+						console.log("File removed: ", file);
+					} else if (err) {
+						throw new Error(err);
+					} else {
+						console.log("File updated: ", file);
+					}
+					file = normDir(dir, file);
+					server.files[file] = data;
 				})
-				file = str + file;
-				server.files[file] = data;
-				console.log("File added: ", file);
-			})
+			}
 		})
 	},
-	start: function(port){
-		port = port || 8080; // default port
+	run: function(port){
+		port = parseInt(port) || server.port; // default port
+
 		(http.createServer(server.requestHandler)).listen(port, function(){
 			// set requestHandler and start listen on port
 			server.running = true;
