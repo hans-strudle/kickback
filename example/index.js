7acc06a2db063c827e84fa5d87ba7cf4a135539f
Hans Strausl <hstrausl@outward.private>
2016-10-07 18:43:28 -0700

    examples done, custom webhooks

---
+++ example/index.js
@@ -0,0 +1,35 @@
+var kickback = require('../server.js');
+
+var qs = require('querystring');
+
+var PORT = 8012;
+
+var dir = 'test-site';
+
+var total = 0;
+
+kickback.customHeaders = {
+	'css/style.css': {
+		'content-type': 'text/css'
+	}
+}
+
+kickback.endpoints = {
+	'index.html': function(url, query){
+		total += parseInt(qs.parse(query).amount);
+		console.log('Total: ', total);
+	},
+	'edit.html': function(url, query, req){
+		var newFile = qs.parse(query).file
+		kickback.files['edit.html'] = '<html><head><title>edit</title><body>' +
+		'<textarea>' + kickback.files[newFile] + '</textarea>' +
+		'</body></html>'
+	}
+}
+
+kickback.ignore.push('test.txt')
+
+kickback.init(dir, function(){
+	console.log(kickback.files);
+	kickback.run(PORT);
+});
