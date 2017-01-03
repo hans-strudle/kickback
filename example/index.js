d3560eaf168f7d2bebfdf4c048de39454e59ea20
hstrausl@outwardinc.com <hstrausl@outwardinc.com>
2017-01-02 16:54:27 -0800

    rewrite

--- example/index.js
+++ example/index.js
@@ -1,36 +1,16 @@
-var kickback = require('../server.js'),
-	path = require('path');
+var kickback = require('kickback');
 
-var qs = require('querystring');
-
-var PORT = 8012;
-
-var dir = 'test-site';
-
-var total = 0;
-
-kickback.customHeaders = {
-	'css/style.css': {
-		'content-type': 'text/css'
+kickback.basedir = __dirname + '/site';
+kickback.map[404] = '/404.html';
+kickback.mapper = function(url, res, cb){
+	if (url.endsWith('/')){
+		url += 'index.html';
 	}
+	return url;
 }
-
-kickback.endpoints = {
-	'index.html': function(url, query){
-		total += parseInt(qs.parse(query).amount);
-		console.log('Total: ', total);
-	},
-	'edit.html': function(url, query, req){
-		var newFile = qs.parse(query).file
-		kickback.files['edit.html'] = '<html><head><title>edit</title><body>' +
-		'<textarea>' + kickback.files[newFile] + '</textarea>' +
-		'</body></html>'
-	}
+kickback.endpoints['/info'] = function(url, req, cb){
+	console.log(cb);
+	cb(4);
 }
-
-kickback.ignore.push('test.txt')
-
-kickback.init(dir, function(){
-	console.log(kickback.files);
-	kickback.run(PORT);
-});
+kickback.start(80);
+console.log(kickback);
---
+++ kickback.js
@@ -0,0 +1,95 @@
+var http = require('http'),
+	URL = require('url'),
+    fs = require('fs');
+
+var kickback = {
+    basedir: '.',
+	quiet: false,
+    map: {
+        '/': '/index.html'
+    },
+    endpoints: {},
+	log: function(...data){
+		if (!kickback.quiet) console.log('kickback: ', data.join(' '));
+	},
+    serve: function(path, cb){
+		kickback.log('grabbing file:',kickback.basedir + path);
+		fs.readFile(kickback.basedir + path, function(err, content){
+			if (err) kickback.log(err);
+			cb(content);
+		})
+    },
+	mapper: function(url,req){
+		if (url.length > 1 && url.endsWith('/')) url = url.substr(0,url.length-1);
+		return kickback.map[url];
+	},
+	headers: function(url, content, cb){
+		var head = {code: 404, headers: {}};
+		if (content){
+			head.code = 200;
+		}
+		if (url.endsWith('.css')){
+			head.headers['Content-Type'] = 'text/css';
+		} else if (url.endsWith('.js')){
+			head.headers['Content-Type'] = 'text/js';
+		}
+		return head;
+	},
+	onRequest: function(url, query, req, cb){
+		// this will run everytime a request is made
+		kickback.log('request path:', url);
+		kickback.log('request query:', query);
+	},
+	onStatus: function(code, cb){
+			return kickback.serve('/' + code + '.html') || ('Error! status code: ' + code);
+	},
+    onRequest: function(req, res){
+		var url  = URL.parse(req.url);
+		var query = qs.parse(url.query);
+		kickback.onRequest(url.pathname, query, req);
+		var url = kickback.mapper(url.pathname, req) || url.pathname;
+		// if no endpoint, just try and serve the file
+		var result = kickback.endpoints[url] || kickback.serve(url, function(result){
+			var header = kickback.headers(url, result, function(header){
+				kickback.sendRes(res, header, result); // serve & header sync
+			}):
+			kickback.sendRes(res, header, result);
+		});
+		// endpoint can be static or a function
+		if (typeof result == 'function') result = result(url, query, req, function(result){
+			var header = kickback.headers(url, result, function(header){
+				kickback.sendRes(res, header, result); // endpoint & header async	
+			});
+			kickback.sendRes(res, header, result); // endpoint async, header sync
+		});
+		var header = kickback.headers(url, result, function(header){
+			kickback.sendRes(res, header, result); // result sync & header async
+		});
+		kickback.sendRes(res, header, result); //both sync
+    },
+	sendRes: function(res, header, result){
+		if (header && result){ // still waiting
+			res.writeHead(header.code, header.headers);
+			res.end(result);
+			return true;
+		}
+		return false;
+	},
+    start: function(port, cb){
+        kickback.server = http.createServer(kickback.onRequest);
+		kickback.server.listen(port, function(){
+			port = port || 80;
+			kickback.log('Listening on port:', port);
+			if (cb) cb(kickback);
+			return kickback;
+		});
+    } 
+};
+
+module.exports = kickback;
+
+if (!module.parent){
+	if (process.argv.indexOf('--quiet') > 0) kickback.quiet = true;
+	kickback.basedir = process.argv[2] || '.';
+	kickback.start(process.argv[3] || 80);
+}
